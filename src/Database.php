56e99676b8e9888609dfdf83cbff71d4a92e26c4
martin bamber <mbamber1986@gmail.com>
2024-11-03 16:48:34 +0000

    Implemented DbQueries trait and reflected changes to match

--- src/Database.php
+++ src/Database.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace LazarusPhp\DatabaseManager;
+use LazarusPhp\DatabaseManager\Traits\DbQueries;
 
 use PDO;
 use PDOException;
@@ -25,6 +26,7 @@ abstract class Database extends DbConfig
     private  $password;
     private  $dbname;
 
+    use DbQueries;
 
     public function __construct()
     {
@@ -71,89 +73,7 @@ abstract class Database extends DbConfig
         return $this;
     }
     
-    public function GenerateQuery($sql=null, $array = [])
-    {
-        !is_null($sql) ? $this->sql = $sql : false;
-        // Get the Params
-        if (!empty($array)) $this->param = $array;
 
-        // Check there is a connection
-        try {
-            $this->stmt = $this->connection->prepare($this->sql);
-            if (!empty($this->param)) $this->BindParams();
-            $this->param = [];
-            $this->stmt->execute();
-            return $this->stmt;
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage() . $e->getCode());
-        }
-    }
-
-    final public function BindParams()
-    {
-        if (!empty($this->param)) {
-            // Prepare code
-            foreach ($this->param as $key => $value) {
-                $type = $this->GetParamTpe($value);
-                $this->stmt->bindValue($key, $value,$type);
-            }
-        }
-    }
-
-    private function GetParamTpe($value)
-    {
-        switch ($value) {
-            case is_bool($value):
-                return PDO::PARAM_BOOL;
-            case is_null($value):
-                return PDO::PARAM_NULL;
-            case is_int($value):
-                return PDO::PARAM_INT;
-            case is_string($value):
-                return PDO::PARAM_STR;
-            default;
-                break;
-        }
-    }
-
-
-    public function AddParams($name, $value)
-    {
-        $this->param[$name] = $value;
-        return $this;
-    }
-
-/**
- * Undocumented function
- *
- * @param [type] $type
- * @return void
- * Use Sql Function to chainload the following functions 
- * Updated 18/06/2024
- */
-    public function One($type = PDO::FETCH_OBJ)
-    {
-        $stmt = $this->GenerateQuery();
-        return $stmt->fetch($type);
-    }
-
-    public function All($type = PDO::FETCH_OBJ)
-    {
-        $stmt = $this->GenerateQuery();
-        return $stmt->fetchAll($type);
-    }
-
-    public function FetchColumn()
-    {
-        $stmt = $this->GenerateQuery();
-        return $stmt->fetchColumn();
-    }
-
-    public function RowCount()
-    {
-        $stmt = $this->GenerateQuery();
-        return $stmt->rowCount();
-    }
 
 
     public function Options()
