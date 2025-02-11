6524f30bdc80148af77e7e28c57552ef69b39d14
martin bamber <mbamber1986@gmail.com>
2025-02-11 23:32:46 +0000

    removed Encryption Simplified codebase

--- src/Database.php
+++ src/Database.php
@@ -8,71 +8,91 @@ use LazarusPhp\SecurityFramework\EncryptionCall;
 class Database
 {
 
-    private static $config;
-    private static  $type;
-    private static $username;
-    private static $hostname;
-    private static $dbname;
-    private static $password;
-    private static ConfigInterface $configInterface;
+    private static $config = [];
 
-    // get the filename;
-    private static $filename;
-    private static $class;
-   
-
-    // Start again here
-
-
-    private static function bindClass(array $class):void
+    protected static function set($name, $value)
     {
-        class_exists($class[0]) ? self::$configInterface = new $class[0](self::$filename) : trigger_error("Class Does not exist");    
+            self::$config[$name] = $value;
     }
 
-    protected static function bindProperties():void
+    protected static function get($name)
     {
-        self::$type = self::$configInterface->setType();
-        self::$hostname = self::$configInterface->setHostname();
-        self::$username = self::$configInterface->setUsername();
-        self::$password = self::$configInterface->setPassword();
-        self::$dbname = self::$configInterface->setDbname();
+            return self::$config[$name];
     }
 
-    public static function instantiate(string $filename, array $class = [PhpWriter::class]):void
-    {
-        // Override $key
-        self::$filename = $filename;
-        if(is_array($class))
+
+        public static function DetectFileType($filename)
         {
-        self::bindClass($class);
+            return pathinfo($filename,PATHINFO_EXTENSION);
         }
-        
+    
+
+    protected static function bindProperties($type="",$hostname="",$username="",$password="",$dbname=""):void
+    {
+        self::set("type",$type);
+        self::set("hostname",$hostname);
+        self::set("username", $username);
+        self::set("password", $password);
+        self::set("dbname", $dbname);
     }
+
+    public static function instantiate(string|array $param)
+    {
+        if(is_array($param))
+        {
+            self::bindProperties($param["type"],$param["hostname"],$param["username"],$param["password"],$param["dbname"]);
+        }
+        elseif(file_exists($param))
+        {       
+                include_once($param);
+                self::bindProperties($type,$hostname,$username,$password,$dbname);
+        }
+        else
+        {
+            trigger_error("Error Occurred : File or array data not found");
+        }
+
+
+    }
+
+    // public static function instantiate(string $filename, array $class = [PhpWriter::class]):void
+    // {  
+    //     // Override $key
+    //     self::$filename = $filename;
+    //     if(is_array($class))
+    //     {
+    //     self::bindClass($class);
+    //     }
+    // }
+
+
 
     protected static function getType()
     {
-        return EncryptionCall::decryptValue(self::$type);
+        return self::$config["type"];
     }
 
     protected static function getHostname()
     {
-        return EncryptionCall::decryptValue(self::$hostname);
+        return self::$config["hostname"];
     }
 
     protected static function getUsername()
     {
-        return EncryptionCall::decryptValue(self::$username);
+        return self::$config["username"];
     }
 
     protected static function getPassword()
     {
-        return EncryptionCall::decryptValue(self::$password);
+        return self::$config["password"];
     }
 
     protected static function getDbName()
     {
-        return EncryptionCall::decryptValue(self::$dbname);
+        return self::$config["dbname"];
     }
+
+ 
 
 
 }
