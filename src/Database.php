c5375ab60216642953e2982ebff6c60c5b87884d
martin bamber <mbamber1986@gmail.com>
2025-03-17 22:19:28 +0000

    Moved QueryBuilder content to Database

--- src/Database.php
+++ src/Database.php
@@ -5,6 +5,7 @@ namespace LazarusPhp\DatabaseManager;
 
 use PDO;
 use PDOException;
+use RuntimeException;
 
 abstract class Database extends Connection
 {
@@ -13,6 +14,8 @@ abstract class Database extends Connection
     protected  $connection;
     protected  $is_connected = false;
     protected $stmt;
+    protected $name;
+    public $lastId;
 
 
     // Params
@@ -26,6 +29,7 @@ abstract class Database extends Connection
             $this->connect();
     }
 
+    // Instantiate the Initial Connection
     private function connect()
     {
         try {
@@ -38,12 +42,107 @@ abstract class Database extends Connection
             throw new PDOException($e->getMessage(), (int)$e->getCode());
         }
     }
+    
+    // get Connection
+    protected function getConnection()
+    {
+        if($this->is_connected){
+        return $this->connection;
+        }
+        else
+        {
+            throw new RuntimeException("Failed to get Database Connection");
+        }
+    }
 
+    // Begin transaction
+    protected function beginTransaction()
+    {
+        $this->getConnection()->beginTransaction();
+    }
 
+    // Commit transactoin
+    protected function commit()
+    {
+        $this->getConnection()->commit();
+    }
 
-   
+    // RollBack a transaction if failed
+    protected function rollback()
+    {
+        return $this->getConnection()->rollback();
+    }
 
+    // Set Prepare Statement
+    protected function prepare(string $sql)
+    {
+        return $this->connection->prepare($sql);
+    }
 
+    // Build and process the Database Query
+    public function save(?string $sql = null, $array = []): mixed
+    { 
+        !is_null($sql) ? $this->sql = $sql : false;
+        // Get the Params
+        if (!empty($array)) $this->param = $array;
+        // Check there is a connection
+        try {
+            $this->stmt = $this->prepare($this->sql);
+            if (!empty($this->param)) $this->bindParams();
+            $this->param = [];
+            $this->beginTransaction();
+            $this->stmt->execute();
+            $this->lastId();
+            $this->commit();
+
+            return $this->stmt;
+        } catch (PDOException $e) {
+            $this->rollback();
+            throw $e;
+        }
+    }
+
+    // Return the last id the inserted value;
+
+       
+    public function lastId()
+    {
+        $this->lastId = $this->getConnection()->lastInsertId();
+    }
+
+    // Param Binding
+
+    protected function bindParams(): void
+    {
+        if (!empty($this->param)) {
+            // Prepare code
+            foreach ($this->param as $key => $value) {
+                $type = $this->getParamType($value);
+                $this->stmt->bindValue($key, $value, $type);
+            }
+        }
+    }
+
+    // Get the Param Type
+    protected function getParamType($value)
+    {
+        switch ($value) {
+            case is_bool($value):
+                return PDO::PARAM_BOOL;
+            case is_null($value):
+                return PDO::PARAM_NULL;
+            case is_int($value):
+                return PDO::PARAM_INT;
+            case is_string($value):
+                return PDO::PARAM_STR;
+            default;
+                break;
+        }
+    }
+
+    // Dsn Options
+
+    
     public function options():array
     {
         $options = [
