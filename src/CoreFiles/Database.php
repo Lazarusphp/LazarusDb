d09f6da3798df452a6a707520f3b74b1a8a61454
martin bamber <mbamber1986@gmail.com>
2025-03-29 23:40:14 +0000

    Recreated Database File to support Persistant and non persistant connection

---
+++ src/CoreFiles/Database.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace LazarusPhp\DatabaseManager\CoreFiles;
+
+use LazarusPhp\DatabaseManager\Connection;
+use PDO;
+use PDOException;
+
+class Database extends Connection
+{
+    // Class implementation goes here
+    protected  $connection;
+    protected  $is_connected = false;
+    protected $stmt;
+    public $lastId;
+
+    public function __construct()
+    {
+        if(self::$isPersistant)
+        {
+            $this->connect();
+        }
+    }
+
+
+    private function connect()
+    {
+        // check for Connection
+        try {
+            // Manage Credentials
+            if(!$this->is_connected){
+                $this->is_connected = true;
+                $this->connection = new PDO($this->dsn(),self::bind("username"), self::bind("password"), $this->options());
+            }
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    protected function getConnection()
+    {
+        if($this->is_connected){
+        return $this->connection;
+        }
+        else
+        {
+            throw new RuntimeException("Failed to get Database Connection");
+        }
+    }
+
+    // Begin transaction
+    protected function beginTransaction()
+    {
+        try {
+            $this->getConnection()->beginTransaction();
+        } catch (PDOException $e) {
+            throw new RuntimeException("Failed to begin transaction: " . $e->getMessage(), (int)$e->getCode());
+        }
+    }
+        // $this->getConnection()->beginTransaction();
+
+    // Commit transactoin
+    protected function commit()
+    {
+        try {
+            $this->getConnection()->commit();
+        } catch (PDOException $e) {
+            throw new RuntimeException("Failed to commit transaction: " . $e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    // RollBack a transaction if failed
+    protected function rollback()
+    {
+        try {
+            $this->getConnection()->rollback();
+        } catch (PDOException $e) {
+            throw new RuntimeException("Failed to rollback transaction: " . $e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    // Set Prepare Statement
+    protected function prepare(string $sql)
+    {
+        return $this->connection->prepare($sql);
+    }
+
+
+
+    // Return the last id the inserted value;
+
+       
+    public function lastId()
+    {
+        return $this->getConnection()->lastInsertId();
+    }
+
+    // Param Binding
+
+    protected function bindParams(): void
+    {
+        if (!empty($this->param)) {
+            // Prepare code
+            foreach ($this->param as $key => $value) {
+                $type = $this->getParamType($value);
+                $this->stmt->bindValue($key, $value, $type);
+            }
+        }
+    }
+
+    // Get the Param Type
+    protected function getParamType($value)
+    {
+        switch ($value) {
+            case is_bool($value):
+                return PDO::PARAM_BOOL;
+            case is_null($value):
+                return PDO::PARAM_NULL;
+            case is_int($value):
+                return PDO::PARAM_INT;
+            case is_string($value):
+                return PDO::PARAM_STR;
+            default;
+                break;
+        }
+    }
+
+    // Dsn Options
+
+    
+    public function options():array
+    {
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        return $options;
+    }
+
+    private function dsn():string
+    {
+        return self::bind("type") . ":host=" . self::bind("hostname") . ";dbname=" . self::bind("dbname");
+    }
+}
