84dd68b8e1d0a4f264d0cfc5887ba5383741a382
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2025-05-24 08:10:52 +0100

    Created Array Control trait file, array control is designed to check values against array

---
+++ src/SharedAssets/Traits/ArrayControl.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace LazarusPhp\LazarusDb\SharedAssets\Traits;
+use LazarusPhp\LazarusDb\SchemaBuilder\Schema; 
+use Exception;
+
+
+trait ArrayControl
+{
+
+    protected $error = [];
+
+
+    public function keyExists($key,$array,$message)
+    {
+        if(array_key_exists($key,$array))
+        {
+            Schema::$progress = false;
+            $this->error[] = $message;
+            return true;
+        }
+        return false;
+    }
+
+    public function loadErrors()
+    {
+            return $this->error;
+
+    }
+
+    public function inArray($key,$array,$message)
+    {
+        if(!in_array($key,$array))
+        {
+             trigger_error($message);
+            exit();
+        }
+    }  
+
+    public function isArray($array,$message)
+    {
+        if(!is_array($array))
+        {    
+            trigger_error($message);
+            exit();
+        }
+    }   
+
+    public function isString($string,$message)
+    {
+        if(!is_string($string))
+        {
+            throw new \Exception($message);
+            exit();
+        }
+    }
+
+    public function isInt($int,$message)
+    {
+        if(!is_int($int))
+        {
+            throw new \Exception($message);
+            exit();
+        }
+    }
+
+    public function isBool($bool,$message)
+    {
+        if(!is_bool($bool))
+        {
+            throw new \Exception($message);
+            exit();
+        }
+    }
+
+    public function isFloat($float,$message)
+    {
+        if(!is_float($float))
+        {
+            throw new \Exception($message);
+            exit();
+        }
+    }   
+
+    public function isNull($null,$message)
+    {
+        if(!is_null($null))
+        {
+            throw new \Exception($message);
+            exit();
+        }
+    }
+    
+    public function isEmpty($array,$message)
+    {
+        if(empty($array))
+        {
+            throw new \Exception($message);
+            exit();
+        }
+    }
+}
