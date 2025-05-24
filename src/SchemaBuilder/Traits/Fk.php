d16f1c33580943fd56ca86960c327c9f0c96811e
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2025-05-24 22:23:19 +0100

    Updated Traits to support new changes

--- src/SchemaBuilder/Traits/Fk.php
+++ src/SchemaBuilder/Traits/Fk.php
@@ -10,12 +10,9 @@ trait Fk
     protected $refTable = [];
     protected $refCol = [];
 
-    protected $ondelete = false;
-    protected $delete = [];
-    protected $onupdate = false;
-    protected $update = [];
-
     protected $constraint = false;
+    protected $delete = [];
+    protected $update = [];
 
     public function constraint(string $column)
     {
@@ -39,8 +36,60 @@ trait Fk
 
     public function references($table,$column)
     {
-        $this->refTable[$this->fkCol] = $table;
-        $this->refCol[$this->fkCol] = $column;
+        if(!$this->keyExists($this->fkCol, $this->refTable, "Reference table for foreign key $this->fkCol already exists"))
+        {
+             $this->refTable[$this->fkCol] = $table;
+             $this->refCol[$this->fkCol] = $column;
+            return $this;
+        }
+        echo "Reference table for foreign key $this->fkCol already exists";
+        return false;
+
+    }
+
+    protected function actions(string $action)
+    {
+        $action = strtoupper($action);
+
+        $supportedActions = [
+            'CASCADE',
+            'RESTRICT',
+            'NULL',
+            'NOACTION',
+            'DEFAULT'
+        ];
+
+
+        $this->inArray($action,$supportedActions,"Action $action is not supported");
+
+        switch($action)
+        {
+            case 'CASCADE':
+                return "CASCADE";
+            case 'RESTRICT':
+                return "RESTRICT";
+            case 'NULL':
+                return "SET NULL";
+            case 'NOACTION':
+                return "NO ACTION";
+            case 'DEFAULT':
+                return "SET DEFAULT";
+        }   
+    }
+    public function onDelete($action="RESTRICT")
+    {
+        $action = $this->actions($action);
+        $this->keyExists($this->fkCol, $this->delete, "Foreign key column $this->fkCol already exists");
+        $this->delete[$this->fkCol] = $action;
+        return $this;
+    }
+
+    public function onUpdate($action="RESTRICT")
+    {
+        $action = $this->actions($action);
+        $this->keyExists($this->fkCol, $this->update, "Foreign key column $this->fkCol already exists");
+        $this->update[$this->fkCol] = $action;
+        // echo $this->update[$this->fkCol];
         return $this;
     }
 
@@ -51,7 +100,13 @@ trait Fk
             foreach($this->fk as $key => $fk)
             {
                 $constraints = $this->constraint === true ? " CONSTRAINT fk_".$fk." ": " ";
-                $this->query[] = "FOREIGN KEY ".$constraints."(".$fk.") REFERENCES ".$this->refTable[$key]."(".$this->refCol[$key].")";
+                $delete = isset($this->delete[$key]) ? $this->delete[$key] : " RESTRICT ";
+                // echo $delete;
+                $update = isset($this->update[$key]) ? $this->update[$key] : " RESTRICT ";
+        
+                $reference = isset($this->refTable[$key]) ? $this->refTable[$key] : exit("Reference table for foreign key $fk does not exist");
+                // echo $update;
+                $this->query[] = "FOREIGN KEY ".$constraints."(".$fk.") REFERENCES  $reference (".$this->refCol[$key].") ON DELETE $delete  ON UPDATE $update";
             }
 
         return implode(",", $this->query);
--- src/SchemaBuilder/Traits/Indexes.php
+++ src/SchemaBuilder/Traits/Indexes.php
@@ -30,10 +30,52 @@ trait Indexes
     }
 
 
-    public function primaryKey()
+    public function primaryKey(array | string $columns= "")
     {
-        self::$primaryKey[$this->name] = $this->name;
-        return $this;
+
+      
+        
+
+        if(is_array($columns) && count($columns) >= 1)
+        {
+            foreach($columns as $column)
+            {
+                if(!$this->keyExists($column, self::$primaryKey, "Primary key column $column already exists"))
+                {
+                    self::$primaryKey[$column] = $column;
+                }
+                else
+                {
+                    trigger_error("Primary key column $column already exists");
+                    exit();
+                }
+            }
+        }
+        elseif(is_string($columns) && !empty($columns))
+        {
+            if(!$this->keyExists($columns, self::$primaryKey, "Primary key column $columns already exists"))
+            {
+                self::$primaryKey[$columns] = $columns;
+            }
+            else
+            {
+                trigger_error("Primary key column $columns already exists");
+                exit();
+            }
+        }
+        else
+        {
+            if(!$this->keyExists($this->name, self::$primaryKey, "Primary key column $this->name already exists"))
+            {
+                self::$primaryKey[$this->name] = $this->name;
+            }
+            else
+            {
+                trigger_error("Primary key column $this->name already exists");
+                exit();
+            }
+            return $this;
+        }
     }
 
     // public function index(string $name)
