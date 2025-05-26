36556562e9453c2253c15beb4072363673960274
martin bamber <mbamber1986@gmail.com>
2025-05-26 08:49:04 +0100

    Added Support to detect table  upon creation, this should cause the script to skip the called class

--- src/SharedAssets/Traits/ArrayControl.php
+++ src/SharedAssets/Traits/ArrayControl.php
@@ -1,111 +1,105 @@
 <?php
 
 namespace LazarusPhp\LazarusDb\SharedAssets\Traits;
-use LazarusPhp\LazarusDb\SchemaBuilder\Schema; 
+use LazarusPhp\LazarusDb\SchemaBuilder\Schema;
+use App\System\Classes\ErrorHandler\ErrorTest;
 use Exception;
 
 
 trait ArrayControl
 {
 
-    public $error = [];
 
-
-    public function keyExists($key,$array,$message)
+    public static function keyExists($key, $array)
     {
-        if(array_key_exists($key,$array))
-        {
-            Schema::$progress = false;
-            $this->error[] = $message;
+        if (array_key_exists($key, $array)) {
+            Schema::$migrationError[] = "Key '$key' exists in the array.";
             return true;
-        }
-        else{
-         Schema::$progress = true;
+        } else {
+            return false;
         }
     }
 
-    public function loadErrors()
+    public static function inArray($key, $array)
     {
-            return $this->error;
-
+        if (in_array($key, $array)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public function inArray($key,$array,$message)
+    public static function isArray($array, $message)
     {
-        if(!in_array($key,$array))
-        {
-            trigger_error($message);
-            Schema::$progress = false;
-            $this->error[] = $message;
-            return true;
-        }
-        else{
-               schema::$progress = true;
-                return false;
-        }
- 
-    }  
-
-    public function isArray($array,$message)
-    {
-        if(!is_array($array))
-        {    
+        if (!is_array($array)) {
             trigger_error($message);
             exit();
         }
-    }   
+    }
 
-    public function isString($string,$message)
+    public static function getType($value)
     {
-        if(!is_string($string))
-        {
+        if (is_string($value)) {
+            return 'string';
+        } elseif (is_int($value)) {
+            return 'int';
+        } elseif (is_bool($value)) {
+            return 'bool';
+        } elseif (is_float($value)) {
+            return 'float';
+        } elseif (is_array($value)) {
+            return 'array';
+        } elseif (is_null($value)) {
+            return 'null';
+        } elseif (is_object($value)) {
+            return 'object';
+        } elseif (is_resource($value)) {
+            return 'resource';
+        } else {
+            return 'unknown';
+        }
+    }
+
+    public static function isString($string)
+    {
+      return is_string($string) ? true : false;
+    }
+
+    public static function isInt($int, $message)
+    {
+        if (!is_int($int)) {
             throw new \Exception($message);
             exit();
         }
     }
 
-    public function isInt($int,$message)
+    public static function isBool($bool, $message)
     {
-        if(!is_int($int))
-        {
+        if (!is_bool($bool)) {
             throw new \Exception($message);
             exit();
         }
     }
 
-    public function isBool($bool,$message)
+    public static function isFloat($float, $message)
     {
-        if(!is_bool($bool))
-        {
+        if (!is_float($float)) {
             throw new \Exception($message);
             exit();
         }
     }
 
-    public function isFloat($float,$message)
+    public static function isNull($null, $message)
     {
-        if(!is_float($float))
-        {
-            throw new \Exception($message);
-            exit();
-        }
-    }   
-
-    public function isNull($null,$message)
-    {
-        if(!is_null($null))
-        {
+        if (!is_null($null)) {
             throw new \Exception($message);
             exit();
         }
     }
-    
-    public function isEmpty($array,$message)
+
+    public static function isEmpty($array, $message)
     {
-        if(empty($array))
-        {
-            throw new \Exception($message);
-            exit();
+        if (empty($array)) {
         }
     }
 }
