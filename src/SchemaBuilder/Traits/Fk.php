a5e0fef5d13b82a5f1582c056951e6007eabf83e
martin bamber <mbamber1986@gmail.com>
2025-07-31 12:15:02 +0100

    Added Support for foreign keys in new DataStructure

--- src/SchemaBuilder/Traits/Fk.php
+++ src/SchemaBuilder/Traits/Fk.php
@@ -2,102 +2,80 @@
 
 namespace LazarusPhp\LazarusDb\SchemaBuilder\Traits;
 
+use App\System\Core\Functions;
 use LazarusPhp\LazarusDb\SchemaBuilder\Schema;
 
 trait Fk
 {
 
+    private static array  $fk = [];
 
-    protected static $fk = [];
-    protected static $refTable = [];
-    protected static $refcol = [];
-    protected static $action = [];
+    
 
+    // tablename ColumnName if constraint is true command 1 and 2 are on update or ondelete
 
-
-    public function processFk()
+    public function fk($table="",$column="",$constraint=false)
     {
-        $table = Schema::getTable();
-      if(!isset(self::$fk[$table]))
-      {
-        // Supports refcol reftable and column
-        self::$fk[$table] = [];
-        // Delete and update and constraint = true or false;
-        self::$action[$table] = [];
-      }
-
-      return true;
-    }
-
-    public function addFk($table,$column)
-    {
-        $name = $this->name;
-        $actions = [
-            $this->name => [
-                "table"=>$table,
-                "column"=>$column
-            ],
-        ];
-        $this->processRequest($this->name,"fk",$actions);
-        return $this;
-    }
-
-    public function isDeleted($action)
-    {
-        $actions = [
-            $this->name => ["onDelete"=>$action],
-        ];
-
+        $actions = ["currentColumn"=>$this->name,"table"=>$table,"column"=>$column,"cont"=>$constraint];
         $this->processRequest($this->name,"fk",$actions);
         return $this;
     }
 
 
-        public function isUpdated($action)
+    public function onUpdate($action = "cascade")
     {
-        $actions = [
-            $this->name => ["onUpdate"=>$action],
-        ];
-
-        $this->processRequest($this->name,"fk",$actions);
+        $actions = ["command"=>"$action"];
+        $this->processRequest($this->name,"fkUpdate",$actions);
         return $this;
     }
 
-
-
-    public function constraint($refTable,$refColumn)
+    public function onDelete($action = "cascade")
     {
-        $table = Schema::getTable();
-        self::$action[$table]["constraint"] = true;
-        $this->fk($refTable,$refColumn); 
-        return $this;
-    }
-
-        public function onUpdate($action)
-    {
-        $table = Schema::getTable();
-        $action = $this->actions($action);
-        self::$action[$table]["update"] = $action;
-        return $this;
-    }
-
-    public function onDelete($action)
-    {
-        $table = Schema::getTable();
-        $action = $this->actions($action);
-        self::$action[$table]["delete"] = $action;
+     
+        $actions = ["command"=>"$action"];
+        $this->processRequest($this->name,"fkDelete",$actions);
         return $this;
     }
 
 
-    public function fk($refTable,$refColumn){
-        $this->processFk();
-        $table = Schema::getTable();
-        self::$fk[$table]["column"] = $this->name;
-        self::$fk[$table]["refTable"] = $refTable;
-        self::$fk[$table]["refColumn"] = $refColumn;
-        return $this;
-    }
+
+
+
+
+
+//     public function constraint($refTable,$refColumn)
+//     {
+//         $table = Schema::getTable();
+//         self::$action[$table]["constraint"] = true;
+//         $this->fk($refTable,$refColumn); 
+//         return $this;
+//     }
+
+//         public function onUpdate($action)
+//     {
+//         $table = Schema::getTable();
+//         $action = $this->actions($action);
+//         self::$action[$table]["update"] = $action;
+//         return $this;
+//     }
+
+//     public function onDelete($action)
+//     {
+//         $table = Schema::getTable();
+//         $action = $this->actions($action);
+//         self::$action[$table]["delete"] = $action;
+//         return $this;
+//     }
+
+
+//     public function fk($refTable,$refColumn){
+//         $this->processFk();
+//         $table = Schema::getTable();
+//         self::$fk[$table]["column"] = $this->name;
+//         self::$fk[$table]["refTable"] = $refTable;
+//         self::$fk[$table]["refColumn"] = $refColumn;
+//         return $this;
+//     }
 
     private function actions(string $action)
     {
@@ -135,30 +113,30 @@ trait Fk
     }
 
 
-public function loadFk()
-{
-    $table = Schema::getTable();
-    if (isset(self::$fk[$table]) && !empty(self::$fk[$table])) {
-        $fkSql = [];
-        $fk = self::$fk[$table];
-        if (isset($fk['column'], $fk['refTable'], $fk['refColumn'])) {
-            $constraint = (isset(self::$action[$table]["constraint"]) && self::$action[$table]["constraint"] === true)
-                ? "CONSTRAINT fk_{$table}_{$fk['column']} "
-                : "";
-            $update = isset(self::$action[$table]["update"]) ? self::$action[$table]["update"] : "RESTRICT";
-            $delete = isset(self::$action[$table]["delete"]) ? self::$action[$table]["delete"] : "RESTRICT";
-            $fkSql[] = "{$constraint} FOREIGN KEY (`{$fk['column']}`) REFERENCES `{$fk['refTable']}`(`{$fk['refColumn']}`) ON DELETE $delete ON UPDATE $update";
-        }
+// public function loadFk()
+// {
+//     $table = Schema::getTable();
+//     if (isset(self::$fk[$table]) && !empty(self::$fk[$table])) {
+//         $fkSql = [];
+//         $fk = self::$fk[$table];
+//         if (isset($fk['column'], $fk['refTable'], $fk['refColumn'])) {
+//             $constraint = (isset(self::$action[$table]["constraint"]) && self::$action[$table]["constraint"] === true)
+//                 ? "CONSTRAINT fk_{$table}_{$fk['column']} "
+//                 : "";
+//             $update = isset(self::$action[$table]["update"]) ? self::$action[$table]["update"] : "RESTRICT";
+//             $delete = isset(self::$action[$table]["delete"]) ? self::$action[$table]["delete"] : "RESTRICT";
+//             $fkSql[] = "{$constraint} FOREIGN KEY (`{$fk['column']}`) REFERENCES `{$fk['refTable']}`(`{$fk['refColumn']}`) ON DELETE $delete ON UPDATE $update";
+//         }
 
-        $colums=[];
-        foreach($fkSql as $sql)
-        {
-            $columns[] = $sql;
-            echo $sql;
-        }
-        $data = implode(", ", $columns);
-        self::$query["fk"]= $data;
-    }
-}
+//         $colums=[];
+//         foreach($fkSql as $sql)
+//         {
+//             $columns[] = $sql;
+//             echo $sql;
+//         }
+//         $data = implode(", ", $columns);
+//         self::$query["fk"]= $data;
+//     }
+// }
 
 }
