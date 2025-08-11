44aba5a7a6681a0e51ebd25b0b443c330a1d003a
martin bamber <mbamber1986@gmail.com>
2025-08-11 10:58:25 +0100

    created new SchemaAction Interface to replace TableInterface

---
+++ src/SchemaBuilder/Interfaces/SchemActionInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LazarusPhp\LazarusDb\SchemaBuilder\Interfaces;
+
+interface SchemActionInterface
+{
+    // Define your method signatures here
+
+    public function tinyInt(string $name);
+    public function int(string $name);
+    public function bigInt(string $name);
+    public function string(string $name,int $value);
+    public function varchar(string $name,int $value);
+    public function text(string $name);
+    public function mediumText(string $name);
+    public function longText(string $name);
+    public function date(string $name);
+    public function dateTime(string $name);
+}
