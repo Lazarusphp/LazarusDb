17b3157eacb6244edeef740ac49b4a6bdebdd84a
martin bamber <mbamber1986@gmail.com>
2025-08-27 10:00:04 +0100

    Readded SchemaCore Temporarily

--- src/SchemaBuilder/CoreFiles/SchemaCore.php
+++ src/SchemaBuilder/CoreFiles/SchemaCore.php
@@ -18,29 +18,66 @@ abstract class SchemaCore extends Database
     protected $name;
     protected static $query = [];
     protected static $table;
+    protected static $param = [];
+    protected $isSelected = false;
+    protected static $allowdBinding = [];
     // Sql Statement
-
     protected static $sql = "";
 
-    // Constructor
-
-    public function __construct()
+      protected function bindParams(): void
     {
-        parent::__construct();
+        if (!empty(self::$param)) {
+            // Prepare code
+            foreach (self::$param as $key => $value) {
+                $type = $this->getParamType($value);
+                $this->stmt->bindValue($key, $value, $type);
+            }
+        }
     }
 
+    // Get the Param Type
+    protected function getParamType($value)
+    {
+        switch ($value) {
+            case is_bool($value):
+                return PDO::PARAM_BOOL;
+            case is_null($value):
+                return PDO::PARAM_NULL;
+            case is_int($value):
+                return PDO::PARAM_INT;
+            case is_string($value):
+                return PDO::PARAM_STR;
+            default;
+                break;
+        }
+    }
+
+    // Unbind
+    private function unbind()
+    {
+        self::$param = [];
+    }
+
+
+    // Save functions.
     protected function save(string $sql = "")
     {
        $sql = !empty($sql) ? $sql : self::$sql; 
-       try {
-           $stmt = $this->prepare($sql);
-           $stmt->execute();
-           return true;
-       } catch (PDOException $e) {
-            SchemaErrors::generate("Failed to Build Schema",["reason",$e->getMessage()]);
-            return false;
-       }
+        try {
+            $this->stmt = $this->prepare($sql);
+            if (!empty(self::$param) && count(self::$param)) $this->bindParams();
+            if($this->stmt->execute())
+            {
+            if($this->isSelected === true)
+            {
+                $this->stmt->closeCursor();
+            }
+            $this->unbind();
+        }
+            return $this->stmt;
+        } catch (PDOException $e) {
+            SchemaErrors::generate("Cannot Write Schema", ["reason"=>$e->getMessage()]);
+        }
    }
-   
 
 }
